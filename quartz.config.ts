d647c13309e66a631b48559eaf7e29259c6a9acf
Ziming Yang <yangzi1ming@126.com>
2025-09-04 22:53:10 +0800

    Update theme.lightMode

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,14 +24,14 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          light: "#fafafa",
+          lightgray: "#e4e5e7",
+          gray: "#9d9d9d",
+          darkgray: "#6a6a6a",
+          dark: "#383a42",
+          secondary: "#4078f2",
+          tertiary: "#50a14f",
+          highlight: "rgba(64, 120, 242, 0.15)",
         },
         darkMode: {
           light: "#282c34",
