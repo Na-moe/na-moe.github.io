637387f953f8ba8ac1d263ec4e309cd9614ed788
Na-moe <yangziming@buaa.edu.cn>
2025-12-13 16:19:40 +0800

    update ACBatch citaitons

--- quartz.config.ts
+++ quartz.config.ts
@@ -73,7 +73,6 @@ const config: QuartzConfig = {
       Plugin.Citations({ 
         bibliographyFile: "./content/ACBatch/ref.bib",
         csl: "./quartz/styles/ieee.csl",
-        linkCitations: true,
       }),
     ],
     filters: [Plugin.RemoveDrafts()],
